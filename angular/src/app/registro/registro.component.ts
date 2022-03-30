30e5ca9b78b6332d50e90231532ff9e93a5a4d3b
Laura <805467@unizar.es>
2022-03-31 00:43:50 +0200

    POR FIN VAN LAS COOKIES

--- angular/src/app/registro/registro.component.ts
+++ angular/src/app/registro/registro.component.ts
@@ -3,6 +3,8 @@ import { FormBuilder, FormGroup, Validators, FormControl, AbstractControl  } fro
 import { Observable } from 'rxjs';
 import { EnviarFormularioService } from '../enviar-formulario.service';
 import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
+import { waitForAsync } from '@angular/core/testing';
+import { AppModule } from '../app.module';
 
 
 @Component({
@@ -16,7 +18,7 @@ export class RegistroComponent  {
   profileForm = this.fb.group({
     nombre: new FormControl('',[
       Validators.required,
-      Validators.minLength(4),
+      Validators.minLength(5),
       Validators.pattern('[A-Za-z0-9-_]+'),
     ]),
     email: new FormControl('',[
@@ -24,36 +26,49 @@ export class RegistroComponent  {
       Validators.email,
     ]),
     password: new FormControl('',[
-      Validators.required,]),
+      Validators.required,
+      Validators.minLength(8),
+    ]),
     password2: new FormControl('',[
       Validators.required,]),
-    //password2: [''],
   });
   
-
   
   constructor(private fb: FormBuilder, private enviar: EnviarFormularioService, private http: HttpClient) {}
-  mycookie:any;
+  
+  cookie:any;       
+  nombre_usuario:any; 
+
+  getNombre_Usuario(nombre:string) {
+    nombre = nombre.split('=')[1];
+    nombre = nombre.split('|')[0];
+    return nombre;
+  }
+
+  getCookie(nombre:string) {
+    nombre = nombre.split('|')[1];
+    nombre = nombre.split(';')[0];
+    return nombre;
+}
+
   onSubmit() {
     var formData: any = new FormData();
     formData.append('nombre', this.profileForm.get('nombre')!.value);
     formData.append('email', this.profileForm.get('email')!.value);
     formData.append('password', this.profileForm.get('password')!.value);
     
-
-    var cookie;
     this.http.post('http://localhost:8090/registro', formData, {observe:'response', responseType:'text'})
         .subscribe({
           next :(response) => {console.log('Respuesta:',response.body),
-                              cookie = response.body,
-                              console.log('Cookie',cookie)
-                              this.mycookie = response.body,
-                              console.log('Cookie',this.mycookie)},
+                              this.cookie = this.getCookie(response.body!),
+                              this.nombre_usuario = this.getNombre_Usuario(response.body!),
+                              localStorage.setItem('cookie', this.cookie),
+                              localStorage.setItem('nombre_usuario', this.nombre_usuario)
+                            },
                               
-          error: (error) => console.log('El error:',error.error),//console.log(error.error), 
-        });
-
- }
+          error: (error) => {alert(error.error)}//console.log('El error:',error.error)}, 
+        });  
+  }
 
 
   get nombre() { return this.profileForm.get('nombre')!; }
@@ -61,8 +76,8 @@ export class RegistroComponent  {
   get password() { return this.profileForm.get('password')!; }
   get password2() { return this.profileForm.get('password2')!; }
 
+
+
 }
 
-export interface Config {
-  respuesta: string;
-}
+ 
