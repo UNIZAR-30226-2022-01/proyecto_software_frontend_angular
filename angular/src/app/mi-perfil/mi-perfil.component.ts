6c8c3b3489b76d00139280d44c50ec9d7194a963
Laura <805467@unizar.es>
2022-04-18 12:26:07 +0200

    medio arreglado los botones

---
+++ angular/src/app/mi-perfil/mi-perfil.component.ts
@@ -0,0 +1,56 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-mi-perfil',
+  templateUrl: './mi-perfil.component.html',
+  styleUrls: ['./mi-perfil.component.css']
+})
+export class MiPerfilComponent implements OnInit {
+   nombre:any;
+    email:any;
+    descripcion:any;
+    partidas_ganadas:any;
+    partidas_totales:any;
+    constructor(private http: HttpClient, private router:Router){}
+    
+    getNombre_Usuario(nombre:string) {
+      nombre = nombre.split('=')[1];
+      nombre = nombre.split('|')[0];
+      return nombre;
+    }
+
+    amigos:any;
+    ngOnInit(): void {
+      this.nombre = this.getNombre_Usuario(document.cookie), 
+      this.http.get<Perfil>('http://localhost:8090/api/obtenerPerfil/'+this.nombre, {observe:'body',withCredentials: true})
+          .subscribe(
+            data => {console.log(data);
+              //this.nombre = data.NombreUsuario,
+              //this.descripcion = data.Biografia,
+              this.partidas_ganadas = data.PartidasGanadas,
+              this.partidas_totales = data.PartidasTotales,
+              this.email = data.Email;
+          })
+      this.descripcion='Apasionado de los juegos de mesa. En mis tiempos libres soy ingeniero'
+    }
+  
+    listaAmigos(){
+      this.http.get<any>('http://localhost:8090/api/listarAmigos', {observe:'body',withCredentials: true})
+          .subscribe(
+            data => {this.amigos = data})
+    }
+  
+    perfil(amigo:string){
+      this.router.navigate(['/perfil', amigo])
+    }
+  }
+  export interface Perfil {
+    NombreUsuario: string;
+    Email: string;
+    Biografia: any;
+    PartidasGanadas: any;
+    PartidasTotales: any;
+  
+  }
