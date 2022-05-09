041cf9c6d9442e53258ca5d5cecbc1f8bb232f37
Sam <git@samueldgv.com>
2022-05-10 00:32:30 +0200

    Llamada de personalizar con integración de fotos de dados y avatares

--- angular/src/app/tienda/tienda.component.ts
+++ angular/src/app/tienda/tienda.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {LlamadasAPI} from "../llamadas-api";
+import {HttpClient} from "@angular/common/http";
+import {Router} from "@angular/router";
+import {DomSanitizer, SafeUrl} from '@angular/platform-browser';
 
 @Component({
   selector: 'tienda',
@@ -7,9 +11,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TiendaComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient, private router:Router, private sanitizer : DomSanitizer){}
+
+  llamadasAPI : LlamadasAPI = new LlamadasAPI(this.http);
+
+  avatar: SafeUrl | null = null;
 
   ngOnInit(): void {
+    //var avatar = this.llamadasAPI.obtenerAvatar(this, "jugador1")
+    this.llamadasAPI.obtenerImagenItem(this, "1")
   }
 
+  cambiarAvatar(blob : any, usuario : string) {
+    const unsafeImg = URL.createObjectURL(blob);
+    this.avatar = this.sanitizer.bypassSecurityTrustUrl(unsafeImg);
+    //this.avatar = window.URL.createObjectURL(blob);
+  }
+
+  introducirImagen(blob : any, id : string) {
+    const unsafeImg = URL.createObjectURL(blob);
+    this.avatar = this.sanitizer.bypassSecurityTrustUrl(unsafeImg);
+    //this.avatar = window.URL.createObjectURL(blob);
+  }
 }
