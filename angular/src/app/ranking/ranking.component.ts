640948f4f9b92a10220297f17b10619f6ea285f8
Laura <805467@unizar.es>
2022-05-13 01:29:20 +0200

    ranking hecho

--- angular/src/app/ranking/ranking.component.ts
+++ angular/src/app/ranking/ranking.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +7,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ranking.component.css']
 })
 export class RankingComponent implements OnInit {
+  //api/ranking Tipo: GET
+  yo:any;
+  miPosicion:any;
+  noSoyTop:any;
+  misPartidasGanadas:any;
+  misPartidasTotales:any; 
 
-  constructor() { }
+  getNombreUsuario(nombre:string) {
+    nombre = nombre.split('=')[1];
+    nombre = nombre.split('|')[0];
+    return nombre;
+  }
 
+  constructor(private http: HttpClient) { }
+  jugadores:any;
+  
   ngOnInit(): void {
+    this.yo = this.getNombreUsuario(document.cookie);
+
+    this.http.get('http://localhost:8090/api/ranking', {observe:'body', responseType:'text', withCredentials: true})
+      .subscribe({
+        next :(response) => {
+          var todosJugadores = JSON.parse(response);
+          var misDatos = todosJugadores.find((element: any) => element.NombreUsuario == this.yo);
+          this.misPartidasGanadas = misDatos!.PartidasGanadas;
+          this.miPosicion = todosJugadores.findIndex((element: any) => element.NombreUsuario == this.yo);
+          
+          if (this.miPosicion > 10){
+            this.noSoyTop = true;
+          }
+          else{
+            this.noSoyTop = false;
+          }
+          this.jugadores = todosJugadores.slice(0,10);
+        }})
+    
+    
   }
 
 }
