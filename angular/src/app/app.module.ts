e7fce7a933d1e8cded16153eba80d02aa9e73232
Sam <git@samueldgv.com>
2022-05-19 01:28:36 +0200

    Corregir rutas

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -36,7 +36,7 @@ import { CartasComponent } from './cartas/cartas.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 const appRoutes: Routes = [
-  {path: 'i', component: InicioComponent},
+  {path: '', component: InicioComponent},
   {path: 'registroUsuario', component: RegistroComponent},
   {path: 'inicioSesion', component: InicioSesionComponent},
   {path: 'tienda', component: TiendaComponent},
@@ -47,7 +47,7 @@ const appRoutes: Routes = [
   {path: 'personalizacion', component: PersonalizacionComponent},
   {path: 'notificaciones', component: NotificacionesComponent},
   {path: 'mapa', component: MapaComponent},
-  {path: '', component: CrearPartidaComponent},
+  {path: 'crearPartida', component: CrearPartidaComponent},
   {path: 'ranking', component: RankingComponent},
   {path: 'amigos', component: AmigosComponent},
   {path: 'identificacion', component: IdentificacionComponent},
