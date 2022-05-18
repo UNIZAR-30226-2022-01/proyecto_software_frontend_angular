a77de54c70d022e3d742a9befa9699538ec8d865
Laura <805467@unizar.es>
2022-05-18 21:22:05 +0200

    topbar juego ->ok

--- angular/src/app/top-bar-juego/top-bar-juego.component.ts
+++ angular/src/app/top-bar-juego/top-bar-juego.component.ts
@@ -1,4 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
+import {LlamadasAPI} from "../llamadas-api";
 
 @Component({
   selector: 'top-bar-juego',
@@ -7,24 +11,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TopBarJuegoComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient, private router: Router) { }
 
   ngOnInit(): void {
   }
 
-  mostrarHistorial(){
-    console.log('Mostrar historial')
-  }
-
-  mostrarAyuda(){
-    console.log('Mostrar ayuda')
-  }
-
   rendirse(){
-    console.log('Mostrar rendirse')
-  }
+    Swal.fire({
+      title: '¿Salir del juego?',
+      text: '¡Rindete ahora y perderás este juego!',
+      showDenyButton: true,
+      confirmButtonText: 'Salir',
+      denyButtonText: `Cancelar`,
+    }).then((result) => {
+      /* Read more about isConfirmed, isDenied below */
+      if (result.isConfirmed) {
+        this.http.post(LlamadasAPI.URLApi+'/api/abandonarPartida', null, { observe:'response', responseType:'text', withCredentials: true})
+          .subscribe({
+            next :(response) => {Swal.fire({
+                                            title: 'Se ha abando la partida con éxito',
+                                            icon: 'success',
+                                            timer: 2000,
+                                            timerProgressBar: true,
+                                          });
 
-  configuracion(){
-    console.log('Mostrar configuracion')
+                                  this.router.navigate(['/lobby'])
+                                },
+            error: (error) => {Swal.fire({
+                                        title: 'Se ha producido un error al abandonar la partida',
+                                        text: error.error,
+                                        icon: 'error',
+                                        })
+                              }
+          });
+      }
+    })
+  
+
   }
 }
+
